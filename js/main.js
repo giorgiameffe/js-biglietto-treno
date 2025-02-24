984d1c842d7e2c709096e29b79aa19a5036849fb
giorgiameffe <giorgiameffe03@gmail.com>
2025-02-24 20:07:56 +0100

    feat: add prompts and full price

---
+++ js/main.js
@@ -0,0 +1,20 @@
+
+// Chiedere all'utente quanti chilometri ha intenzione di percorrere
+
+const kmToDo = prompt('Quanti chilometri vuoi percorrere?');
+console.log(kmToDo);
+
+// Chiedere all'utente quanti anni ha
+
+const myAge = prompt('Quanti anni hai?');
+console.log(myAge);
+
+// Definire il prezzo pieno del biglietto
+
+const fullPrice = kmToDo * 0.21;
+
+
+
+
+
+
